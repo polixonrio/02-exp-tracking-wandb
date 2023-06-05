ae1d655c5028be5fa026e5ea04d93bebb63049ac
xilihulu <xilihulu@pm.me>
2023-06-06 00:49:12 +0530

    lmfao

---
+++ homework-wandb/lmao.py
@@ -0,0 +1,6 @@
+python sweep.py --wandb_project mlz --wandb_entity polixonrio --data_artifact "polixonrio/mlz/NYC-Taxi:v0"
+
+python sweep.py \
+  --wandb_project <WANDB_PROJECT_NAME> \
+  --wandb_entity <WANDB_USERNAME> \
+  --data_artifact
